cb7e7e70e86cb06c1715df14e5ada7ceb00cadf4
songl <song_pku@foxmail.com>
2018-10-22 00:10:46 +0800

    ld

---
+++ init.d
@@ -0,0 +1,3 @@
+init.o: kern/init.c inc/stdio.h inc/stdarg.h inc/string.h inc/types.h \
+ inc/assert.h inc/x86.h kern/monitor.h kern/console.h kern/pmap.h \
+ inc/memlayout.h inc/mmu.h kern/kclock.h
